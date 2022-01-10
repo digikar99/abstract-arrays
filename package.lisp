7be53dbc488d0baf8ce9dfc0d82330a13e8a6855
digikar99 <shubhamkarayare@gmail.com>
2022-01-10 09:17:04 +0530

    [int] rearrange package and pre-package

--- package.lisp
+++ package.lisp
@@ -1,31 +1,8 @@
 (cl:in-package #:abstract-arrays/defpackage)
-#.(cl:let ((export-symbols '(#:abstract-array
-                             #:abstract-array-class
-
-                             #:array-storage
-                             #:array-storage-ref
-                             #:array-storage-set
-
-                             #:array-dimensions
-                             #:abstract-array-dimensions
-                             #:array-dimension
-                             #:array-rank
-                             #:array-element-type
-                             #:array-total-size
-                             #:arrayp
-
-                             #:aref
-                             #:row-major-aref
-
-                             #:define-array-class
-                             #:define-array-specializations
-                             #:define-array-specialization-type
-                             #:array-type-element-type
-                             #:array-type-rank)))
-
-    `(uiop:define-package :abstract-arrays
-         (:mix :cl :polymorphic-functions)
-       (:shadow ,@export-symbols)
-       (:export ,@export-symbols)))
+(polymorphic-functions.defpackage:defpackage :abstract-arrays
+  (:use :cl)
+  (:shadowing-import-exported-symbols :polymorphic-functions)
+  #.(cons :shadow (export-symbols))
+  #.(cons :export (export-symbols)))
 
 (in-package #:abstract-arrays)
--- pre-package.lisp
+++ pre-package.lisp
@@ -3,3 +3,30 @@
 (defpackage #:abstract-arrays/defpackage
   (:use :cl)
   (:intern #:export-symbols))
+
+(in-package #:abstract-arrays/defpackage)
+
+(defun export-symbols ()
+  '(#:abstract-array
+    #:abstract-array-class
+
+    #:array-storage
+    #:array-storage-ref
+    #:array-storage-set
+
+    #:array-dimensions
+    #:abstract-array-dimensions
+    #:array-dimension
+    #:array-rank
+    #:array-element-type
+    #:array-total-size
+    #:arrayp
+
+    #:aref
+    #:row-major-aref
+
+    #:define-array-class
+    #:define-array-specializations
+    #:define-array-specialization-type
+    #:array-type-element-type
+    #:array-type-rank))
