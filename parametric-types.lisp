c632536b345f6051901889d0f79ec37f7827762f
digikar99 <shubhamkarayare@gmail.com>
2023-06-10 23:55:29 +0530

    [fix] surround *array-class-type-alist* in eval-when

--- parametric-types.lisp
+++ parametric-types.lisp
@@ -1,7 +1,8 @@
 (in-package :abstract-arrays)
 
-(defvar *array-class-type-alist* ()
-  "An ALIST used by POLYMORPHIC-FUNCTIONS:SPECIALIZING-TYPE-OF to return the
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defvar *array-class-type-alist* ()
+    "An ALIST used by POLYMORPHIC-FUNCTIONS:SPECIALIZING-TYPE-OF to return the
 appropriate type associated with the array class. The CAR of each entry of
 the alist should be the class or class-name of the concerned array object,
 while the CDR should be a symbol or a function. If the CDR is a symbol,
@@ -9,7 +10,7 @@ then it forms the CAR of the returned type-specifier; if it is a function,
 then the function should take the array-object as the input and return
 a symbol to be used as the CAR of the type-specifier.
 
-See dense-arrays/src/types.lisp for an instance of such a function.")
+See dense-arrays/src/types.lisp for an instance of such a function."))
 
 (polymorphic-functions:defpolymorph polymorphic-functions:specializing-type-of
     ((object abstract-arrays:abstract-array))
